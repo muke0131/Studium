f6715b29cb11f8e4aed0ed18347f547c45f95172
Mukul Nayak <mukulnayak2004@gmail.com>
2024-06-19 06:29:56 +0530

    Models added

---
+++ Server/models/CourseModel.js
@@ -0,0 +1,50 @@
+const mongoose=require("mongoose");
+
+const courseSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    courseDescription:{
+        type:String,
+        trim:true
+    },
+    instructor:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    WhatYouWillLearn:{
+        type:String
+    },
+    courseContent:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Section"
+        }
+    ],
+    ratingAndReviews:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"RatingAndReview"
+        }
+    ],
+    price:{
+        type:Number
+    },
+    thumbnail:{
+        type:String
+    },
+    tag:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Tag"
+    },
+    studentsEnrolled:[{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"User"
+    }]
+})
+
+module.exports=mongoose.model("Course",courseSchema);
---
+++ Server/utils/mailSender.js
@@ -0,0 +1,27 @@
+const nodemailer=require("nodemailer");
+
+const mailSender=async(email,title,body)=>{
+    try{
+        let transpoter=nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass:process.env.MAIL_PASS
+            }
+        })
+
+        let info=await transpoter.sendMail({
+            from:"Studium",
+            to:`${email}`,
+            subject:`${title}`,
+            html:`${body}`
+        })
+        console.log(info);
+        return info;
+    }
+    catch(error){
+        console.log(error.message)
+    }
+}
+
+module.exports=mailSender;
